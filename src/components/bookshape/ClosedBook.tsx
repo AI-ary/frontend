ed26cfac0f576af8a27639f85a1aff98f8375a51
gs0428 <lider28@naver.com>
2023-08-04 15:57:19 +0900

    refactor : 책 넘기는 부분 className 지정

--- src/components/bookshape/ClosedBook.tsx
+++ src/components/bookshape/ClosedBook.tsx
@@ -1,32 +1,33 @@
 import { useNavigate } from 'react-router-dom';
 import * as S from '../../styles/bookshape/closedbook.style'
-import { RiArrowLeftSLine } from "react-icons/ri";
+import { RiArrowLeftSLine } from 'react-icons/ri';
 import Bookmark from './Bookmark';
 
+
 const ClosedBook = ({ children }: React.PropsWithChildren) => {
-    const path = window.location.pathname;
-    const home = { name: '홈', path : path }
-    const write = { name: '일기 쓰기', path: path }
-    const navigate = useNavigate();
-    return(
-        <S.Container>
-            <S.Left />
-            <S.Flip>
-                <S.BackBtn path={path} onClick={()=>navigate(-1)}>
-                    <RiArrowLeftSLine size={70} />
-                </S.BackBtn>
-                <S.Mid>
-                    {children}
-                </S.Mid>
-                <S.Right />
-                <Bookmark />
-                {/* <S.LabelWrap>
+  const path = window.location.pathname;
+  const home = { name: '홈', path : path }
+  const write = { name: '일기 쓰기', path: path }
+  const navigate = useNavigate();
+  return(
+    <S.Container className='slide'>
+      <S.Left/>
+      <S.Flip className='flip'>
+        <S.BackBtn path={path} onClick={()=>navigate(-1)}>
+          <RiArrowLeftSLine size={70} />
+        </S.BackBtn>
+        <S.Mid>
+          {children}
+        </S.Mid>
+        <S.Right />
+        <Bookmark />
+        {/* <S.LabelWrap>
                     <S.Label name={'홈'} path={path} onClick={()=>navigate('/')}>홈</S.Label>
                     <S.Label name={'일기 쓰기'} path={path} >일기 쓰기</S.Label>
                 </S.LabelWrap> */}
-            </S.Flip>
-        </S.Container>
-    )
+      </S.Flip>
+    </S.Container>
+  )
 }
 
 export default ClosedBook
