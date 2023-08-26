41c8bb0924b9d501764fef1238c7417eff2e8ae8
minjilee <budlalswl1129@gmail.com>
2023-08-26 18:16:50 +0900

    design: 테마 선택 퍼블리싱

--- src/styles/bookshape/closedbook.style.ts
+++ src/styles/bookshape/closedbook.style.ts
@@ -1,4 +1,6 @@
 import styled, { css } from "styled-components"
+import { ReactComponent as HiddenPalette } from '../../../public/images/palette.svg';
+import { ReactComponent as ShowPalette } from '../../../public/images/palette2.svg';
 
 export const Container = styled.div`
   display : flex;
@@ -65,4 +67,57 @@ export const BehindWrap = styled.div`
 export const FrontWrap = styled.div`
   position : absolute;
   display : flex;
+`
+
+export const StyledHiddenPalette = styled(HiddenPalette)`
+  position: absolute;
+  left: 40px;
+  top: 40%;
+  cursor: pointer;
+  z-index: 99;
+`
+export const StyledShowPalette = styled(ShowPalette)`
+  position: absolute;
+  left: 40px;
+  top: 40%;
+  cursor: pointer;
+  z-index: 99;
+`
+
+export const ToggleTheme = styled.ul`
+  width: 200px;
+  height: 300px;
+  position: absolute;
+  top: 28%;
+  left: 60px;
+  transition: all 0.3s;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  cursor: pointer;
+  &.show-menu {
+    visibility: visible;
+    opacity: 1;
+    
+  }
+  &.hide-menu {
+    visibility: hidden;
+    opacity: 0;
+  }
+
+  > li {
+    width: 65px;
+    height: 65px;
+    overflow: hidden;
+    > img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  > li:last-child{
+    position: absolute;
+    left: 100px;
+  }
 `
